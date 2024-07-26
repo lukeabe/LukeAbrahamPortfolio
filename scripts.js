0d56f8da469484427e88af432e95f598ff7a196b
lukeabraham <lukeabraham1175@gmail.com>
2024-07-25 22:12:27 -0500

    Update

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Message sent!');
-    });
-});
